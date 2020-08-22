3df614c4c4b8c2dd3403e2d00237d4708412ffc2
Dylan Bremner <61300825+bremnerdc@users.noreply.github.com>
2020-08-22 12:02:23 -0700

    Updates

---
+++ precache-manifest.a1c0ccbd19179fe9d5c61b027843feda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3b30f02f570d20d9e9d9",
+    "url": "/MyReactPortfolio/static/css/main.b7c1248a.chunk.css"
+  },
+  {
+    "revision": "3b30f02f570d20d9e9d9",
+    "url": "/MyReactPortfolio/static/js/main.3b30f02f.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/MyReactPortfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "d32ff011c1b6b868c7f0",
+    "url": "/MyReactPortfolio/static/js/runtime~main.d32ff011.js"
+  },
+  {
+    "revision": "39738104ad9767088ffda314059d7a5c",
+    "url": "/MyReactPortfolio/index.html"
+  }
+];
